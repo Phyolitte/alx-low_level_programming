b4af88d939fe18b1a88f1a17d34ebb430f36631f
Laila Ibnziaten <Lailaibnziaten00@gmail.com>
2023-05-15 06:45:57 -0700

    task 6

--- 0x0B-malloc_free/100-argstostr.c
+++ 0x0B-malloc_free/100-argstostr.c
@@ -1,102 +1,80 @@
 #include "main.h"
+		
 #include <stdlib.h>
-
-int word_len(char *str);
-int count_words(char *str);
-char **strtow(char *str);
-
+		
 /**
- * word_len - Locates the index marking the end of the
- *            first word contained within a string.
- * @str: The string to be searched.
- *
- * Return: The index marking the end of the initial word pointed to by str.
+		
+ * argstostr - main entry
+		
+ * @ac: int input
+		
+ * @av: double pointer array
+		
+ * Return: 0
+		
  */
-int word_len(char *str)
+		
+char *argstostr(int ac, char **av)
+		
 {
-	int index = 0, len = 0;
+		
+	int i, n, r = 0, l = 0;
+		
+	char *str;
+		
 
-	while (*(str + index) && *(str + index) != ' ')
+		
+	if (ac == 0 || av == NULL)
+		
+		return (NULL);
+		
+
+		
+	for (i = 0; i < ac; i++)
+		
 	{
-		len++;
-		index++;
+		
+		for (n = 0; av[i][n]; n++)
+		
+			l++;
+		
 	}
+		
+	l += ac;
+		
 
-	return (len);
+		
+	str = malloc(sizeof(char) * l + 1);
+		
+	if (str == NULL)
+		
+		return (NULL);
+		
+	for (i = 0; i < ac; i++)
+		
+	{
+		
+	for (n = 0; av[i][n]; n++)
+		
+	{
+		
+		str[r] = av[i][n];
+		
+		r++;
+		
+	}
+		
+	if (str[r] == '\0')
+		
+	{
+		
+		str[r++] = '\n';
+		
+	}
+		
+	}
+		
+	return (str);
+		
 }
 
-/**
- * count_words - Counts the number of words contained within a string.
- * @str: The string to be searched.
- *
- * Return: The number of words contained within str.
- */
-int count_words(char *str)
-{
-	int index = 0, words = 0, len = 0;
-
-	for (index = 0; *(str + index); index++)
-		len++;
-
-	for (index = 0; index < len; index++)
-	{
-		if (*(str + index) != ' ')
-		{
-			words++;
-			index += word_len(str + index);
-		}
-	}
-
-	return (words);
-}
-
-/**
- * strtow - Splits a string into words.
- * @str: The string to be split.
- *
- * Return: If str = NULL, str = "", or the function fails - NULL.
- *         Otherwise - a pointer to an array of strings (words).
- */
-char **strtow(char *str)
-{
-	char **strings;
-	int index = 0, words, w, letters, l;
-
-	if (str == NULL || str[0] == '\0')
-		return (NULL);
-
-	words = count_words(str);
-	if (words == 0)
-		return (NULL);
-
-	strings = malloc(sizeof(char *) * (words + 1));
-	if (strings == NULL)
-		return (NULL);
-
-	for (w = 0; w < words; w++)
-	{
-		while (str[index] == ' ')
-			index++;
-
-		letters = word_len(str + index);
-
-		strings[w] = malloc(sizeof(char) * (letters + 1));
-
-		if (strings[w] == NULL)
-		{
-			for (; w >= 0; w--)
-				free(strings[w]);
-
-			free(strings);
-			return (NULL);
-		}
-
-		for (l = 0; l < letters; l++)
-			strings[w][l] = str[index++];
-
-		strings[w][l] = '\0';
-	}
-	strings[w] = NULL;
-
-	return (strings);
-}
